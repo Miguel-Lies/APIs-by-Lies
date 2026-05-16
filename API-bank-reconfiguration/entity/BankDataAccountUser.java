202b1365337133f8b6825fb08cbef8573091edc3
Miguel-Lies <miguelmendes2031@gmail.com>
2026-05-16 11:20:34 -0300

    feat: nova classe com as roles dos usuarios

--- API-bank-reconfiguration/entity/BankDataAccountUser.java
+++ API-bank-reconfiguration/entity/BankDataAccountUser.java
@@ -1,9 +1,15 @@
 package com.enterprise.bank_lies.entity;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Getter
@@ -13,24 +19,64 @@ import java.time.LocalDate;
 @Builder
 @Entity
 @Table(name = "AccountUser")
-public class BankDataAccountUser {
+public class BankDataAccountUser implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name",length = 80,nullable = false)
+    @Column(name = "name", length = 80, nullable = false)
     private String name;
 
-    @Column(name = "email",length = 90,nullable = false,unique = true)
+    @Column(name = "email", length = 90, nullable = false, unique = true)
     private String email;
 
-    @Column(name = "numberOfUser",nullable = false,unique = true)
-    private String number;
+    @Column(name = "number", nullable = false, unique = true)
+    private String numberOfUser;
 
-    @Column(name = "password",length = 20,nullable = false)
+    @Column(name = "password", length = 20, nullable = false)
     private String password;
 
-    @Column(name = "date_of_birth",nullable = false)
+    @Column(name = "date_of_birth", nullable = true)
     private LocalDate dateOfBirth;
-}
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Roles> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public @Nullable String getPassword(){
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
