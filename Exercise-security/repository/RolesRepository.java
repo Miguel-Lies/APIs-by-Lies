e0710a534e41140d635b9f5a59837280688694e7
Miguel-Lies <miguelmendes2031@gmail.com>
2026-05-19 11:13:17 -0300

    remove pasta

--- Exercise-security/repository/RolesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.of.security.lies.repository;
-
-import com.study.of.security.lies.entity.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolesRepository extends JpaRepository<Roles, Integer>{
-}
